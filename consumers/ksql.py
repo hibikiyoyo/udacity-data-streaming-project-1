c71bd81098ca0f77a448aa3502ffe447ee3c5141
Trung <trung@Hibikiyoyo-2.local>
2024-07-15 19:44:43 +0700

    fix typo

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -33,9 +33,9 @@ CREATE TABLE turnstile (
 );
 
 CREATE TABLE turnstile_summary
-WITH (VALUE_FORMAT = 'JSON',) AS
+WITH (VALUE_FORMAT = 'JSON') AS
     SELECT station_id , COUNT(station_id) AS count
-    FROM turntile
+    FROM turnstile
     GROUP BY station_id;
 """
 
